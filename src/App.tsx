2768219687e4fd5df255039bf9a43aacae4f5adf
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-10-07 17:20:31 +0800

    add skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React, { ReactElement } from "react";
-import "./App.css";
-import { Switch, Route } from "react-router";
-import Home from "./Screens/Home";
+import React, { ReactElement } from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router';
+import Home from './screens/Home';
+import Dashboard from './screens/Dashboard';
 
 const App: React.FC = (): ReactElement => {
   return (
     <React.Fragment>
       <Switch>
         <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/dashboard" component={Dashboard} />
       </Switch>
     </React.Fragment>
   );
