524f80bd65fcd27d77c09371cf14288a06e52d38
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-10-08 08:46:58 +0800

    Fix hours alignment

--- src/components/registerBusiness/Hours.tsx
+++ src/components/registerBusiness/Hours.tsx
@@ -1,6 +1,12 @@
 import React, { ReactElement } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Grid, Switch, FormControl, Select } from '@material-ui/core';
+import {
+  FormControl,
+  Select,
+  Typography,
+  Box,
+  Button,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) => ({
   '@global': {
@@ -18,52 +24,60 @@ const Hours: React.FC = (): ReactElement => {
   const classes = useStyles();
   return (
     <form className={classes.form} noValidate={true}>
-      <Grid container={true} spacing={3}>
-        <Grid item={true} xs={3}>
-          Monday
-        </Grid>
-        <Grid item={true} xs={3}>
-          <Switch
-            value="checkedB"
-            color="primary"
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-          />
-        </Grid>
-        <Grid item={true} xs={3}>
-          <FormControl variant="outlined" fullWidth={true}>
-            <Select
-              native={true}
-              fullWidth={true}
-              inputProps={{
-                name: 'from',
-                id: 'outlined-age-native-simple',
-              }}
-            >
-              <option value={10}>5:00 AM</option>
-              <option value={20}>6:00 AM</option>
-              <option value={30}>7:00 AM</option>
-            </Select>
-          </FormControl>
-          to
-          <FormControl variant="outlined" fullWidth={true}>
-            <Select
-              native={true}
-              fullWidth={true}
-              inputProps={{
-                name: 'from',
-                id: 'outlined-age-native-simple',
-              }}
-            >
-              <option value={10}>5:00 PM</option>
-              <option value={20}>6:00 PM</option>
-              <option value={30}>7:00 PM</option>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item={true} xs={3}>
-          Apply to all
-        </Grid>
-      </Grid>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Box m={1}>
+          <Typography color="primary" style={{ fontWeight: 'bold' }}>
+            Monday
+          </Typography>
+        </Box>
+        <Box m={1}>
+          <Box display="flex" flexDirection="row" alignItems="center">
+            <Box m={1}>
+              <FormControl variant="outlined">
+                <Select
+                  native={true}
+                  inputProps={{
+                    name: 'timeTo',
+                  }}
+                >
+                  <option value={10}>5:00 PM</option>
+                  <option value={20}>6:00 PM</option>
+                  <option value={30}>7:00 PM</option>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box m={1}>
+              <Typography color="primary" style={{ fontWeight: 'bold' }}>
+                to
+              </Typography>
+            </Box>
+            <Box m={1}>
+              <FormControl variant="outlined">
+                <Select
+                  native={true}
+                  inputProps={{
+                    name: 'timeTo',
+                  }}
+                >
+                  <option value={10}>5:00 PM</option>
+                  <option value={20}>6:00 PM</option>
+                  <option value={30}>7:00 PM</option>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+        </Box>
+        <Box m={1}>
+          <Button color="primary" variant="contained">
+            Apply to all
+          </Button>
+        </Box>
+      </Box>
     </form>
   );
 };
